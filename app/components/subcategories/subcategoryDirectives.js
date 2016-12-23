bf09eea21a0199a375e39cf20b2045d38ef15439
Jorge Reyes Jr <reyes.junior.27@gmail.com>
2016-12-22 16:17:41 -0800

    app/components/subcategories/subcategoryDirectives.js[ updated templateUrl's to reflect new file structure]

--- app/components/subcategories/subcategoryDirectives.js
+++ app/components/subcategories/subcategoryDirectives.js
@@ -2,27 +2,27 @@
   app.directive( 'subcategoryBreadcrumb' , function() {
     return {
       restrict: 'E',
-      templateUrl: 'js/directives/subcategories/breadcrumb.html'
+      templateUrl: 'app/components/subcategories/breadcrumb.html'
     };
   });
 
   app.directive( 'subcategoryImagePanel', function() {
     return {
       restrict: 'E',
-      templateUrl: 'js/directives/subcategories/subcategory-image-panel.html'
+      templateUrl: 'app/components/subcategories/subcategory-image-panel.html'
     };
   });
 
   app.directive( 'subcategoryDescriptionPanel', function() {
     return {
       restrict: 'E',
-      templateUrl: 'js/directives/subcategories/subcategory-description-panel.html'
+      templateUrl: 'app/components/subcategories/subcategory-description-panel.html'
     };
   });
 
-  app.directive( 'productListingPanel', function() {
+    app.directive( 'subcategoryListingPanel', function() {
     return {
       restrict: 'E',
-      templateUrl: 'js/directives/subcategories/product-listing-panel.html'
+      templateUrl: 'app/components/subcategories/subcategory-listing-panel.html'
     };
   });
